c6ea33236f8190b76bbabd71b8cf1a6ad2cbf2d2
Roberto Ludwig <robertoldw@gmail.com>
2023-10-06 15:03:12 -0300

    add plop

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,8 @@ module.exports = {
         '!src/app/**',
         '!src/lib/registry.tsx',
         '!src/types/**',
-        '!src/styles/**'
+        '!src/styles/**',
+        '!src/**/stories.tsx'
     ],
     setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
     modulePaths: ['<rootDir>/src/'],
